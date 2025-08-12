b92c6a6b09663126b9688ed67aaea395a3a9381e
Gregory Vettorel <gregory.vettorel@destiny.eu>
2025-08-12 09:41:01 +0200

    Removed unused variable for eslint

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 export default function Step({ step, alt }) {
