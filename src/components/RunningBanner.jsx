88d19a323fef44c6926e70c185b4ffce6d23efd8
Ilian Bosserz <iil.bossserez@gmail.com>
2025-09-02 16:02:33 +0200

    Refactor RunningBanner component layout and update text content

--- src/components/RunningBanner.jsx
+++ src/components/RunningBanner.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "../index.css"
 import "../styles/running-banner.css";
 
 export default function RunningBanner() {
   return (
-    <div className="running-banner">
-      <div className="running-banner__inner">
+    <div className="running-banner section-pad">
+      <div className="running-banner__inner section-pad">
         <span className="running-banner__text">
-         VISUAL DESIGN FOR DIGITAL EXPERIENCES
+     Dikke voeten bepalen de wet
         </span>
       </div>
     </div>
